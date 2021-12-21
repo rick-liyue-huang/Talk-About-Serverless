3d7722c5ff6e2b737fb4e24136800bdebcaa2cad
rick-liyue-huang <rick.liyue.huang@gmail.com>
2021-12-21 23:22:33 +1100

    feat: refactor the webpack config to MPA project

---
+++ src/admin.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css'
+
+ReactDOM.render(
+	<React.StrictMode>
+		<div>admin page</div>
+	</React.StrictMode>,
+	document.getElementById('root')
+);
+
